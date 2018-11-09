bbaf4ebe45d71fc1fb5343d144f236a70ddd4796
Davy Landman <davy.landman@swat.engineering>
2018-11-09 22:56:31 +0100

    Split iteration and upgrade

--- src/main/java/UpgradeIndexes.java
+++ src/main/java/UpgradeIndexes.java
@@ -5,28 +5,39 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexUpgrader;
 import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 
 public class UpgradeIndexes {
     public static void main(String[] args) throws IOException {
         Path sourcePath = new File(args[0]).toPath();
-        System.out.println("Upgrading indexes in: " + sourcePath);
+        System.out.println("Reading indexes from: " + sourcePath);
+        List<Directory> indexes = new ArrayList<>();
         Files.walkFileTree(sourcePath, new SimpleFileVisitor<Path>() {
             @Override
             public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
                 if (file.toString().endsWith(".cfe")) {
                     FSDirectory dir = FSDirectory.open(file.getParent());
-                    try {
-                        new IndexUpgrader(dir, new IndexWriterConfig(null), true).upgrade();
+                    // try to open it to see if it is corrupt
+                    try (DirectoryReader ireader = DirectoryReader.open(dir)) {
+                        indexes.add(dir);
                     } catch (IOException e) {
-                        System.err.println("!! Could not update index: " + file.getParent() + "(" + e.getMessage() + ")");
+                        System.err.println("!! Corrupt index found: " + file.getParent() + "(" + e.getMessage() + ")");
                     }
                 }
                 return super.visitFile(file, attrs);
             }
         });
+        System.out.println("Upgrading " + indexes.size() + " indexes");
+        for (Directory ix : indexes) {
+            new IndexUpgrader(ix, new IndexWriterConfig(null), true).upgrade();
+            ix.close();
+        }
     }
 
 }
